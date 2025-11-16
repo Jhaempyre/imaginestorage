c8a02fb60f46e6d2df0d420ae3dae017fcb5e755
Aashish Jha <jhaashish.ajha@gmail.com>
2025-11-16 13:07:24 +0530

    unneccesary files and folder removed

--- backend/src/models/index.ts
+++
@@ -1,6 +0,0 @@
-// Export all models from a single entry point
-export { User, IUser } from './user';
-export { File, IFile } from './file';
-
-// Re-export mongoose types for convenience
-export { Document, Schema, Types } from 'mongoose';
