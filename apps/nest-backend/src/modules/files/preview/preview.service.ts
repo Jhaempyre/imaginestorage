475a09a3de22ea4eca2a6c917ccfcf42dc47c3a9
Avinash-sord12k <61863628+Avinash-sord12k@users.noreply.github.com>
2025-11-17 05:43:14 +0530

    chore: tryied using load esm utility

--- apps/nest-backend/src/modules/files/preview/preview.service.ts
+++ apps/nest-backend/src/modules/files/preview/preview.service.ts
@@ -9,6 +9,14 @@ import { AudioPreviewHandler } from "./handlers/audio.handler";
 // import { PdfPreviewHandler } from "./handlers/pdf.handler";
 // import { OfficePreviewHandler } from "./handlers/office.handler";
 // import { FallbackPreviewHandler } from "./handlers/fallback.handler";
+// import type * as FileTypeNS from "file-type";
+import { loadEsm } from "load-esm";
+
+async function getFileTypeFromFile(): Promise<any> {
+  // const module = await (eval(`import('file-type')`) as Promise<any>);
+  const module = await loadEsm<typeof import("file-type")>("file-type");
+  return module.fileTypeFromFile;
+}
 
 @Injectable()
 export class PreviewService {
@@ -26,7 +34,9 @@ export class PreviewService {
 
   async generatePreview(tempFilePath: string) {
     // detect file type via magic bytes
-    const { fileTypeFromFile } = await import("file-type");
+    // const { fileTypeFromFile } = await getFileType();
+    // const { fileTypeFromFile } = await import("file-type");
+    const fileTypeFromFile = await getFileTypeFromFile();
     const detected = await fileTypeFromFile(tempFilePath);
     const ext = detected?.ext || path.extname(tempFilePath).slice(1);
     const mime = detected?.mime || "application/octet-stream";
