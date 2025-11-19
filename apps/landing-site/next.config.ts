430634e4ea04132966a417e4f3c11d074da5faf2
Aashish Jha <jhaashish.ajha@gmail.com>
2025-11-20 00:05:57 +0530

    The files and confinguration of next.js is changed for dist/serving only html

--- apps/landing-site/next.config.ts
+++ apps/landing-site/next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: 'export',  // Export as static HTML
+  images: {
+    unoptimized: true  // Required for static export
+  }
 };
 
 export default nextConfig;
